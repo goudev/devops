0a982ad9e80b1af32a77223c2deaaa519835c773
Jose Gouveia Júnior <gouveiaeu@gmail.com>
2023-01-15 17:13:50 -0300

    feat: Validação do carregamento do arquivo de env

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 
 . .env
 
-source scripts/install-git.sh
+if [ $? = 0 ]; then
+    echo "Verifique o arquivo de variaveis de ambiente. Não foi possível carregalo."
+    exit 2;
+fi
 
-#source scripts/install-k3s.sh
-#source scripts/install-helm.sh
-#source scripts/install-cert-manager.sh
-#source scripts/install-nginx-ingress.sh
-#source scripts/install-rancher.sh
+source scripts/install-k3s.sh
+source scripts/install-helm.sh
+source scripts/install-cert-manager.sh
+source scripts/install-nginx-ingress.sh
+source scripts/install-rancher.sh
+source scripts/install-git.sh
+source scripts/install-drone.sh
+source scripts/install-registry.sh
