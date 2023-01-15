fa01775a5fa9566f16197562fcb6c1640323af41
Jose Gouveia Júnior <gouveiaeu@gmail.com>
2023-01-15 17:04:24 -0300

    Novo script de instalação do Drone

---
+++ scripts/install-drone.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export GITEA_SERVER_URL
+export LETSENCRYPT_EMAIL
+export DRONE_ADMIN
+export DRONE_GIT_PASSWORD
+export DRONE_GIT_USERNAME
+export DRONE_RPC_SECRET
+export DRONE_SERVER_HOST
+export DRONE_SERVER_PROTO
+export DRONE_USER_CREATE
+export REGISTRY_URL
+
+echo -n "Instalando serviço do Drone"
+envsubst < artefatos/drone.yaml > /tmp/drone.yaml
+delete -f /tmp/drone.yaml
+kubectl apply -f /tmp/drone.yaml > /tmp/droneinstall.log 2>&1
+if [ $? = 0 ]; then
+    echo "[OK]"
+    kubectl -n git rollout status deploy/drone
+else
+    echo "[ERRO]"
+    echo
+    echo "Erro ao instalar o serviço do Drone"
+    echo
+    cat /tmp/droneinstall.log
+fi
