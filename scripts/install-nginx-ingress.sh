37260526c8597d2f2ae806348796a23d5c65b899
Júnior Gouveia <77864380+goudev@users.noreply.github.com>
2023-01-15 15:11:47 -0300

    feat: Novo script para setup do Nginx Ingress

---
+++ scripts/install-nginx-ingress.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define a versão do Nginx Ingress
+NGINX_INGRESS_RELEASE="release-3.0"
+
+echo -n "Instalando o Nginx Ingress "
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/ns-and-sa.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/rbac/rbac.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/default-server-secret.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/nginx-config.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/ingress-class.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/crds/k8s.nginx.org_virtualservers.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/crds/k8s.nginx.org_virtualserverroutes.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/crds/k8s.nginx.org_transportservers.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/crds/k8s.nginx.org_policies.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/common/crds/k8s.nginx.org_globalconfigurations.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/deployment/nginx-ingress.yaml > /dev/null 2>&1
+kubectl apply -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINX_INGRESS_RELEASE}/deployments/daemon-set/nginx-ingress.yaml > /dev/null 2>&1
+echo "[OK]"
